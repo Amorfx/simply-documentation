273346bc1b2362065d335787c3d8b13ab62ecdaa
Clément Décou <clementdecou@MacBook-Pro-de-Clement.local>
2022-01-13 17:30:46 +0100

    fix(doc): fix edit page link

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -22,14 +22,10 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/Amorfx/simply-documentation/tree/main/documentation',
         },
         blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          showReadingTime: true
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
